5e2b49073e2f2fd4e9c5dbb47f27452a921120ad
Sensei <abu.hasan.5173@gmail.com>
2025-11-06 11:17:25 +0530

    Fixed a bug in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,9 +12,9 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <BrowserRouter>
       <App />
-    </ClerkProvider>{" "}
-  </BrowserRouter>
+    </BrowserRouter>
+  </ClerkProvider>
 );
